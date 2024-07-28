c3c06633197a5ae727531856e67ed47b2056c691
Jules Clements <jules.clements@middleware.co.nz>
2024-07-29 09:23:47 +1200

    Use custom NPM build

--- opennz/docusaurus.config.js
+++ opennz/docusaurus.config.js
@@ -8,20 +8,23 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Open Standard New Zealand',
+  tagline: 'Kiwis can Fly',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://opennz.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cdaf', // Usually your GitHub org/user name.
+  projectName: 'opennz.org', // Usually your repo name.
+
+  // https://docusaurus.io/docs/deployment
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
